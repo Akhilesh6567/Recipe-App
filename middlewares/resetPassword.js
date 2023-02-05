32c6ac954b6026ed6b7e916cc1b9606ff650d6d2
sumikendhar-gitter <sumikendhar.vvf119@gmail.com>
2023-02-05 23:40:12 +0500

    I added reset password module added reset screen * added reset middleware * added route for reset password

---
+++ middlewares/resetPassword.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const baseUrl='https://recipe-maker-34e87-default-rtdb.firebaseio.com';
+export const resetPassword = async ({id,password}) => {
+    try {
+        const usersUrl = `${baseUrl}/users/${id}.json`;
+        const user=await axios.get(usersUrl);
+        if(user.data.password===password){
+            alert("New password cannot be the same as the old password");
+            return false;
+        }
+        await axios.patch(usersUrl, {password});
+        alert("Password changed successfully");
+        return true;
+    }
+    catch (error) {
+        alert(error);
+        return false;
+    }
+}
